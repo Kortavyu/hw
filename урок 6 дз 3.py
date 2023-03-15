57074e165178ec03f4eddf3cdd0ff88df0a17126
Kortavyu <124869853+Kortavyu@users.noreply.github.com>
2023-03-15 22:41:10 +0300

    Add files via upload

---
+++ урок 6 дз 3.py
@@ -0,0 +1,7 @@
+nik_name  =  list ( input ( 'Ввести логин:' ))
+для  я  в  nik_name :
+    если  я  не  в  «1234567890qwertyuiopasdfghjklzxcvbnm» :
+        print ( 'Неверный символ:' , i )
+        перерыв
+еще :
+    print ( 'Вы зарегистрированы!' )
